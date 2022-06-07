8db7e11759a67bd21c5af71199ddfd64bb993ce0
Sthefanye <sthefanye_olive@outlook.com>
2022-06-07 17:29:43 -0400

    Reoganize packages

--- app/src/main/java/com/example/appcolorblindness/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/appcolorblindness/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.appcolorblindness
+package com.example.appcolorblindness.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appcolorblindness.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
