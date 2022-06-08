c1e45f10e7350dccfca61ae2792100c26130281b
Sthefanye <sthefanye_olive@outlook.com>
2022-06-07 22:29:41 -0400

    Add input name screen

---
+++ app/src/main/java/com/example/appcolorblindness/ui/fragments/ChooseGameFragment.kt
@@ -0,0 +1,20 @@
+package com.example.appcolorblindness.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.appcolorblindness.databinding.FragmentChooseGameBinding
+class ChooseGameFragment : Fragment() {
+    private lateinit var binding: FragmentChooseGameBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentChooseGameBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
