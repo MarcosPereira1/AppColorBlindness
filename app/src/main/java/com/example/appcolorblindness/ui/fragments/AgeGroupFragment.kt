54ad3c384585edaf0cc65265dc019c57cb676f1d
Sthefanye <sthefanye_olive@outlook.com>
2022-06-08 02:12:14 -0400

    Add age group screen

--- app/src/main/java/com/example/appcolorblindness/ui/fragments/AgeGroupFragment.kt
+++ app/src/main/java/com/example/appcolorblindness/ui/fragments/AgeGroupFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.appcolorblindness.R
 import com.example.appcolorblindness.databinding.FragmentAgeGroupBinding
 
 class AgeGroupFragment : Fragment() {
@@ -16,6 +18,13 @@ class AgeGroupFragment : Fragment() {
     ): View {
         binding = FragmentAgeGroupBinding.inflate(inflater, container, false)
 
+        navigateToAgeGroup()
         return binding.root
+    }
+
+    private fun navigateToAgeGroup() {
+        binding.ibArrowBack.setOnClickListener {
+            findNavController().navigate(R.id.action_fragAgeGroup_to_fragInputName)
+        }
     }
 }
