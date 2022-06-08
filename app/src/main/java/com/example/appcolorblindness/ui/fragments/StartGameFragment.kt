636ee878e4fcd72e213076576f58cc8e54afbee4
Sthefanye <sthefanye_olive@outlook.com>
2022-06-08 01:06:50 -0400

    Add start game screen

--- app/src/main/java/com/example/appcolorblindness/ui/fragments/StartGameFragment.kt
+++ app/src/main/java/com/example/appcolorblindness/ui/fragments/StartGameFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.appcolorblindness.R
 import com.example.appcolorblindness.databinding.FragmentStartGameBinding
 
 class StartGameFragment: Fragment() {
@@ -16,6 +18,17 @@ class StartGameFragment: Fragment() {
     ): View {
         binding = FragmentStartGameBinding.inflate(inflater, container, false)
 
+        navigateToInputName()
         return binding.root
+    }
+
+    private fun navigateToInputName() {
+        binding.ibPlay.setOnClickListener {
+            findNavController().navigate(R.id.action_fragStartGame_to_fragInputName)
+        }
+
+        binding.ibArrowBack.setOnClickListener {
+            findNavController().navigate(R.id.action_fragStartGame_to_fragAboutGame)
+        }
     }
 }
