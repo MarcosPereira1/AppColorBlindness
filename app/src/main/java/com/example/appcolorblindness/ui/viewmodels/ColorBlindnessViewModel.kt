a3b9c180664cf884eb856c8a661e6115335373e1
Sthefanye <sthefanye_olive@outlook.com>
2022-06-12 15:58:37 -0400

    Add view model in project

---
+++ app/src/main/java/com/example/appcolorblindness/ui/viewmodels/ColorBlindnessViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.appcolorblindness.ui.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ColorBlindnessViewModel : ViewModel() {
+    val usernameLiveData: MutableLiveData<String> by lazy {
+        MutableLiveData("")
+    }
+
+    val has5yearLiveData: MutableLiveData<Boolean> by lazy {
+        MutableLiveData()
+    }
+}
